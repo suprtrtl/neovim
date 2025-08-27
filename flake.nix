6d748f37612f8b3cac335478ca03f430e95a0581
suprtrtl <owen.rodway@gmail.com>
2025-08-27 14:23:31 -0600

    flake.nix 3.0

--- flake.nix
+++ flake.nix
@@ -1,7 +1,17 @@
 {
   description = "My Neovim Config Flake";
 
-  outputs = {self}: {
-    defaultPackage.x86_64-linux = self;
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
+    packages.x86_64-linux = {
+      defualt = nixpkgs.lib.makeWrapper {
+        name = "nvim-config";
+        paths = [./.];
+      };
+    };
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.default;
   };
 }
