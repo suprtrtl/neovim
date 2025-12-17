889e71ed957bc50c71ffc50c8134119fb3ca9075
suprtrtl <owen.rodway@gmail.com>
2025-12-16 23:16:51 -0700

    theme fix?

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -1,2 +1,9 @@
-vim.cmd('colorscheme onedark')
+function Colorize(color)
+	color = color or "onedark"
+	vim.cmd.colorscheme(color)
 
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+vim.schedule(Colorize)
