5421fed0236c344775c3e8f07e8c3220efa50b74
suprtrtl <owen.rodway@gmail.com>
2025-12-17 10:40:53 -0700

    treesitter fix?

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	dependencies = { "OXY2DEV/markview.nvim" },
+	-- dependencies = { "OXY2DEV/markview.nvim" },
 	build = ":TSUpdate",
 	lazy = false,
 	config = function()
@@ -26,46 +26,46 @@ return {
 			highlight = { enable = true },
 		}
 
-		local markview = require("markview")
-		local presets = require("markview.presets")
-
-		markview.setup {
-			markdown = {
-				headings = presets.headings.glow,
-				horizontal_rules = presets.horizontal_rules.thin,
-				tables = presets.tables.rounded,
-				list_items = {
-					shift_width = function(_, _)
-						return 4
-					end,
-				},
-			},
-			modes = { "n", "c" },
-			hybrid_modes = { "n" },
-			callbacks = {
-				on_enable = function(_, win)
-					vim.wo[win].conceallevel = 2
-					vim.wo[win].concealcursor = "c"
-				end,
-			},
-		}
-
-		require("markview.extras.checkboxes").setup({
-			default = "X",
-			remove_style = "disable",
-			states = {
-				{ " ", "/", "X" },
-				{ "<", ">" },
-				{ "?", "!", "*" },
-				{ '"' },
-				{ "l", "b", "i" },
-				{ "S", "I" },
-				{ "p", "c" },
-				{ "f", "k", "w" },
-				{ "u", "d" },
-			},
-		})
-		require("markview.extras.headings").setup()
-		require("markview.extras.editor").setup()
+		-- local markview = require("markview")
+		-- local presets = require("markview.presets")
+		--
+		-- markview.setup {
+		-- 	markdown = {
+		-- 		headings = presets.headings.glow,
+		-- 		horizontal_rules = presets.horizontal_rules.thin,
+		-- 		tables = presets.tables.rounded,
+		-- 		list_items = {
+		-- 			shift_width = function(_, _)
+		-- 				return 4
+		-- 			end,
+		-- 		},
+		-- 	},
+		-- 	modes = { "n", "c" },
+		-- 	hybrid_modes = { "n" },
+		-- 	callbacks = {
+		-- 		on_enable = function(_, win)
+		-- 			vim.wo[win].conceallevel = 2
+		-- 			vim.wo[win].concealcursor = "c"
+		-- 		end,
+		-- 	},
+		-- }
+		--
+		-- require("markview.extras.checkboxes").setup({
+		-- 	default = "X",
+		-- 	remove_style = "disable",
+		-- 	states = {
+		-- 		{ " ", "/", "X" },
+		-- 		{ "<", ">" },
+		-- 		{ "?", "!", "*" },
+		-- 		{ '"' },
+		-- 		{ "l", "b", "i" },
+		-- 		{ "S", "I" },
+		-- 		{ "p", "c" },
+		-- 		{ "f", "k", "w" },
+		-- 		{ "u", "d" },
+		-- 	},
+		-- })
+		-- require("markview.extras.headings").setup()
+		-- require("markview.extras.editor").setup()
 	end,
 }
