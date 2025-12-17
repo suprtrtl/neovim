a539a36278af5d973b20ca0e9b8986061826015e
suprtrtl <owen.rodway@gmail.com>
2025-12-17 11:16:35 -0700

    treesitter fix?

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,126 +1,73 @@
--- return {
--- 	"nvim-treesitter/nvim-treesitter",
--- 	-- dependencies = { "OXY2DEV/markview.nvim" },
--- 	build = ":TSUpdate",
--- 	lazy = false,
--- 	config = function()
--- 		require("nvim-treesitter.configs").setup {
--- 			ensure_installed = {
--- 				"rust",
--- 				"go",
--- 				"c",
--- 				"zig",
--- 				"javascript",
--- 				"typescript",
--- 				"lua",
--- 				"vim",
--- 				"vimdoc",
--- 				"query",
--- 				"markdown",
--- 				"markdown_inline",
--- 				"latex",
--- 				"typst",
--- 			},
--- 			sync_install = false,
--- 			auto_install = true,
--- 			highlight = { enable = true },
--- 		}
---
--- 		-- local markview = require("markview")
--- 		-- local presets = require("markview.presets")
--- 		--
--- 		-- markview.setup {
--- 		-- 	markdown = {
--- 		-- 		headings = presets.headings.glow,
--- 		-- 		horizontal_rules = presets.horizontal_rules.thin,
--- 		-- 		tables = presets.tables.rounded,
--- 		-- 		list_items = {
--- 		-- 			shift_width = function(_, _)
--- 		-- 				return 4
--- 		-- 			end,
--- 		-- 		},
--- 		-- 	},
--- 		-- 	modes = { "n", "c" },
--- 		-- 	hybrid_modes = { "n" },
--- 		-- 	callbacks = {
--- 		-- 		on_enable = function(_, win)
--- 		-- 			vim.wo[win].conceallevel = 2
--- 		-- 			vim.wo[win].concealcursor = "c"
--- 		-- 		end,
--- 		-- 	},
--- 		-- }
--- 		--
--- 		-- require("markview.extras.checkboxes").setup({
--- 		-- 	default = "X",
--- 		-- 	remove_style = "disable",
--- 		-- 	states = {
--- 		-- 		{ " ", "/", "X" },
--- 		-- 		{ "<", ">" },
--- 		-- 		{ "?", "!", "*" },
--- 		-- 		{ '"' },
--- 		-- 		{ "l", "b", "i" },
--- 		-- 		{ "S", "I" },
--- 		-- 		{ "p", "c" },
--- 		-- 		{ "f", "k", "w" },
--- 		-- 		{ "u", "d" },
--- 		-- 	},
--- 		-- })
--- 		-- require("markview.extras.headings").setup()
--- 		-- require("markview.extras.editor").setup()
--- 	end,
--- }
---
---
-
 return {
 	"nvim-treesitter/nvim-treesitter",
+	-- dependencies = { "OXY2DEV/markview.nvim" },
+	build = ":TSUpdate",
+	lazy = false,
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- A list of parser names, or "all"
+		require("nvim-treesitter.configs").setup {
 			ensure_installed = {
-				"vimdoc",
+				"rust",
+				"go",
+				"c",
+				"zig",
 				"javascript",
 				"typescript",
-				"c",
 				"lua",
-				"rust",
-				"jsdoc",
-				"bash",
-				"go",
+				"vim",
+				"vimdoc",
+				"query",
+				"markdown",
+				"markdown_inline",
+				"latex",
+				"typst",
 			},
-
-			-- Install parsers synchronously (only applied to `ensure_installed`)
 			sync_install = false,
-
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
 			auto_install = true,
+			highlight = { enable = true },
+		}
 
-			indent = {
-				enable = true,
+		local markview = require("markview")
+		local presets = require("markview.presets")
+
+		markview.setup {
+			markdown = {
+				headings = presets.headings.glow,
+				horizontal_rules = presets.horizontal_rules.thin,
+				tables = presets.tables.rounded,
+				list_items = {
+					shift_width = function(_, _)
+						return 4
+					end,
+				},
 			},
-
-			highlight = {
-				-- `false` will disable the whole extension
-				enable = true,
-				disable = function(lang, buf)
-					if lang == "html" then
-						print("disabled")
-						return true
-					end
-
-					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						vim.notify(
-							"File larger than 100KB treesitter disabled for performance",
-							vim.log.levels.WARN,
-							{ title = "Treesitter" }
-						)
-						return true
-					end
+			modes = { "n", "c" },
+			hybrid_modes = { "n" },
+			callbacks = {
+				on_enable = function(_, win)
+					vim.wo[win].conceallevel = 2
+					vim.wo[win].concealcursor = "c"
 				end,
 			},
+		}
+
+		require("markview.extras.checkboxes").setup({
+			default = "X",
+			remove_style = "disable",
+			states = {
+				{ " ", "/", "X" },
+				{ "<", ">" },
+				{ "?", "!", "*" },
+				{ '"' },
+				{ "l", "b", "i" },
+				{ "S", "I" },
+				{ "p", "c" },
+				{ "f", "k", "w" },
+				{ "u", "d" },
+			},
 		})
+		require("markview.extras.headings").setup()
+		require("markview.extras.editor").setup()
 	end,
 }
+
+
