ea2c2eafefd45e36c009ab76b844cf074f909302
suprtrtl <owen.rodway@gmail.com>
2025-12-16 23:28:09 -0700

    treesitter fix?

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,6 +9,7 @@ return {
 	end,
 
 	config = function()
+		require('nvim-treesitter').setup()
 		require 'nvim-treesitter.configs'.setup {
 			-- A list of parser names, or "all" (the listed parsers MUST always be installed)
 			ensure_installed = {
