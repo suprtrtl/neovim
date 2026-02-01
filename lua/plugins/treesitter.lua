f574217b316363150dedc22569b79bcc5154920f
suprtrtl <owen.rodway@gmail.com>
2026-01-31 23:27:34 -0700

    treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	build = ":TSUpdate",
 	lazy = false,
 	config = function()
-		require("nvim-treesitter.configs").setup {
+		require("nvim-treesitter").setup {
 			-- ensure_installed = {
 			-- 	"rust",
 			-- 	"go",
