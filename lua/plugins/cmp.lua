43e14025f62cedad217c9efd45fc0e9e9cd9b308
suprtrtl <owen.rodway@gmail.com>
2026-01-31 23:42:12 -0700

    fancy cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -13,6 +13,35 @@ return {
 		require('luasnip.loaders.from_vscode').lazy_load()
 		luasnip.config.setup {}
 
+
+		local kind_icons = {
+			Text = "",
+			Method = "󰆧",
+			Function = "󰊕",
+			Constructor = "",
+			Field = "󰇽",
+			Variable = "󰂡",
+			Class = "󰠱",
+			Interface = "",
+			Module = "",
+			Property = "󰜢",
+			Unit = "",
+			Value = "󰎠",
+			Enum = "",
+			Keyword = "󰌋",
+			Snippet = "",
+			Color = "󰏘",
+			File = "󰈙",
+			Reference = "",
+			Folder = "󰉋",
+			EnumMember = "",
+			Constant = "󰏿",
+			Struct = "",
+			Event = "",
+			Operator = "󰆕",
+			TypeParameter = "󰅲",
+		}
+
 		cmp.setup {
 			snippet = {
 				expand = function(args)
@@ -25,6 +54,21 @@ return {
 				documentation = cmp.config.window.bordered(),
 			},
 
+			formatting = {
+				format = function(entry, vim_item)
+					-- Kind icons
+					vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
+					-- Source
+					vim_item.menu = ({
+						buffer = "[Buffer]",
+						nvim_lsp = "[LSP]",
+						luasnip = "[LuaSnip]",
+						nvim_lua = "[Lua]",
+						latex_symbols = "[LaTeX]",
+					})[entry.source.name]
+					return vim_item
+				end
+			},
 			mapping = cmp.mapping.preset.insert {
 				['<C-n>'] = cmp.mapping.select_next_item(),
 				['<C-p>'] = cmp.mapping.select_prev_item(),
