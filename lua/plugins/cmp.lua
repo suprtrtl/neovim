d1551ae71c05a860a99e39069fa95c700c27dcf0
suprtrtl <owen.rodway@gmail.com>
2026-01-31 23:45:56 -0700

    fancy cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -50,8 +50,14 @@ return {
 			},
 
 			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
+				completion = cmp.config.window.bordered({
+					border = 'rounded', -- or 'single', 'double', 'shadow'
+					winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+				}),
+				documentation = cmp.config.window.bordered({
+					border = 'rounded',
+					winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,Search:None",
+				}),
 			},
 
 			formatting = {
